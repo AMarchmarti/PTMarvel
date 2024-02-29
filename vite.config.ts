a2aeecfc939955af657e6a7765afe3dca93e2dbf
AMarchmarti <marchmarti.antoni@gmail.com>
2024-02-29 15:51:27 +0100

    Added new config and added enviroment apo key

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+import requireTransform from "vite-plugin-require-transform";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { fileURLToPath } from "url";
+
+// eslint-disable-next-line import/no-unused-modules
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), reactRefresh(), requireTransform({})],
+    server: {
+      port: 5555,
+      open: true,
+      proxy: {},
+    },
+    build: {
+      outDir: "./dist",
+      emptyOutDir: true,
+      rollupOptions: {
+        input: "./index.html",
+      },
+      commonjsOptions: {
+        include: [/aliceonboarding/, /node_modules/],
+      },
+    },
+    optimizeDeps: {
+      include: ["aliceonboarding"],
+    },
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".cjs"],
+    },
+  };
+});
