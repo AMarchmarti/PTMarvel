a3e91dbc1d246810c31f288e6e56eb99e4ce9164
AMarchmarti <marchmarti.antoni@gmail.com>
2024-03-04 15:19:58 +0100

    feature[searchInput]: fix stles

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,18 +1,18 @@
-import { useContext } from 'react'
-import marvelLogo from '@/assets/marvel-logo.png';
-import { FavoritesContext } from '../../../context/FavoriteContext';
-import GlobalIcon from '../../Icons/GlobalIcon';
-import { useNavigate } from 'react-router-dom';
-import './Header.css';
+import { useContext } from "react"
+import { useNavigate } from "react-router-dom";
 
-type locationMarvelT = 'favorites' | 'home';
+import { FavoritesContext } from "../../../context/FavoriteContext";
+import GlobalIcon from "../../Icons/GlobalIcon";
+import "./Header.css";
+
+type LocationMarvelT = "favorites" | "home";
 interface LocationMarvel {
     favorites: string;
     home: string;
 }
 const locationMarvel: LocationMarvel = {
-    favorites: '/favorites',
-    home: '/',
+    favorites: "/favorites",
+    home: "/",
 
 };
 
@@ -21,13 +21,13 @@ const Header = () => {
     const { favorites } = useContext(FavoritesContext)
     const navigate = useNavigate();
     const disabled = favorites.length === 0;
-    const handleNavigate = (location: locationMarvelT) => {
+    const handleNavigate = (location: LocationMarvelT) => {
         navigate(locationMarvel[location]);
     }
 
     return <header >
-        <GlobalIcon className='header-logo' iconName="MarvelLogo" onClick={() => handleNavigate('home')} />
-        <button className='header-favorites' onClick={() => handleNavigate('favorites')} disabled={disabled} style={{ cursor: !disabled ? 'pointer' : "not-alloed" }}>
+        <GlobalIcon className='header-logo' iconName="MarvelLogo" onClick={() => handleNavigate("home")} />
+        <button className='header-favorites' onClick={() => handleNavigate("favorites")} disabled={disabled} style={{ cursor: !disabled ? "pointer" : "not-alloed" }}>
             <GlobalIcon iconName="HeartIconSelected" size="XS" />
             {!disabled && <span className='header-favorites-count'>{favorites.length}</span>}
         </button>
